46e674edcbfdbf1b762a2af458e611198e814104
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 19:42:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM series
+  "SELECT authors.name, subgenres.name FROM author
   JOIN series ON series.subgenre_id = subgenres.id"
 end
 
