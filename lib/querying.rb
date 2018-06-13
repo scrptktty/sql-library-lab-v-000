f4f7feaddd8bc2a03a5275f2b011ac9ed08f3e85
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 19:57:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN series ON series.id = characters.series_id
-  GROUP BY characters.species
-  ORDER BY count(*) DESC
-  LIMIT 1"
+  GROUP BY characters.species"
+  # ORDER BY count(*) DESC
+  # LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
