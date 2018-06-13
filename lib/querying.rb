04b93abbd81bad0470c975024489e4e9079aa28e
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 19:58:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
+  "SELECT series.title. characters.species FROM series
   JOIN series ON series.id = characters.series_id
   GROUP BY characters.species"
   # ORDER BY count(*) DESC
