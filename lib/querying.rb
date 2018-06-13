fe876c70bffb4bed07083cf0052caaa3b4b8e252
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 19:33:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, count(*) FROM characters
-  GROUP BY characters.species
-  LIMIT 1"
+  GROUP BY characters.species"
 end
 
 def select_name_and_series_subgenres_of_authors
