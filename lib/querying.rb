454cbe55e7320dc345a37e546e2ea3917cde98bb
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 19:45:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,8 +20,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name FROM authors
+  JOIN series ON series.subgenre_id = subgenres.id
   GROUP BY authords.name"
-  # JOIN series ON series.subgenre_id = subgenres.id
 end
 
 def select_series_title_with_most_human_characters
