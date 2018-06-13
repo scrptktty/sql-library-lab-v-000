92fea312bba68f9139bbc90dce8f8a303cc425ef
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 18:56:58 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+);
