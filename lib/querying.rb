d8563b70ac65f107da876e67c687e9d16e1816a6
Kachel Wittig <kjwittig@gmail.com>
2018-06-13 20:04:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,10 +26,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
+  "SELECT series.title, SUM(characters.species) as species_number FROM series
   JOIN characters ON characters.series_id = series.id
-  GROUP BY series.title
-  ORDER BY SUM(characters) DESC
+  ORDER BY species_number DESC
   LIMIT 1"
 end
 
